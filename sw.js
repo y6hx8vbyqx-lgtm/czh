94f46d71ef9414b8112e761759169e5f8845733e
y6hx8vbyqx-lgtm <y6hx8vbyqx@privaterelay.appleid.com>
2026-02-02 16:45:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-index.html:206 ✅ Firebase инициализирован
-index.html:184 ✅ ServiceWorker зарегистрирован
-sw.js:30 📦 Кеширование статических файлов
-sw.js:1 Uncaught (in promise) TypeError: Failed to execute 'addAll' on 'Cache': Request failed
-sw.js:30 📦 Кеширование статических файлов
-sw.js:1 Uncaught (in promise) TypeError: Failed to execute 'addAll' on 'Cache': Request failed
+
